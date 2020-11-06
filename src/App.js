ed214c4a910acd16b64fb49d73bb8113b2aa3312
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-11-05 20:20:18 -0600

    crud empleados

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
-import Alumno from "./components/Alumno";
-import "./App.css";
+import React from 'react';
+import Alumno from './components/Alumno';
+import Empleado from './components/Empleado';
+import './App.css';
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="container p-4">
       <div className="row">
         <Alumno />
+        <Empleado />
       </div>
       <ToastContainer />
     </div>
--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDdWe4RaYuuTlRUgi0GAlbFWYem7dV7iUI",
-    authDomain: "reactcrudfirebase-73847.firebaseapp.com",
-    databaseURL: "https://reactcrudfirebase-73847.firebaseio.com",
-    projectId: "reactcrudfirebase-73847",
-    storageBucket: "reactcrudfirebase-73847.appspot.com",
-    messagingSenderId: "783525311206",
-    appId: "1:783525311206:web:81038ac7bb061f53f08127"
-  };
-  const fb =  firebase.initializeApp(firebaseConfig);
-  export const db = fb.firestore();
-
+  apiKey: 'AIzaSyCVgWkYuXZUU8FTmmaEp_vVDvPnQRJTIic',
+  authDomain: 'crudreactfirebase-5f307.firebaseapp.com',
+  databaseURL: 'https://crudreactfirebase-5f307.firebaseio.com',
+  projectId: 'crudreactfirebase-5f307',
+  storageBucket: 'crudreactfirebase-5f307.appspot.com',
+  messagingSenderId: '309390802675',
+  appId: '1:309390802675:web:751a83eebcf1d1c0c2c186',
+  measurementId: 'G-0C6VY6GPKY',
+};
+const fb = firebase.initializeApp(firebaseConfig);
+export const db = fb.firestore();
